b9e87d57366e91d5ef2fa9ed34400d16f7ee79e8
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-01-17 11:20:29 +0100

    fix: autocomplete response count limit was not respected

--- src/commands/presenceShift/onAutoComplete.ts
+++ src/commands/presenceShift/onAutoComplete.ts
@@ -4,9 +4,10 @@ import { getMembers } from "../../db/actions/getMembers";
 export async function onAutoComplete(interaction: AutocompleteInteraction) {
 	const focused = interaction.options.getFocused();
 	const choices = getMembers().sort();
-	const filtered = choices.filter((choice) => choice.startsWith(focused));
+	let filtered = choices.filter((choice) => choice.startsWith(focused));
 
-	filtered.concat(choices.filter((choice) => !choice.startsWith(focused) && choice.includes(focused)));
+	filtered = filtered.concat(choices.filter((choice) => !choice.startsWith(focused) && choice.includes(focused)));
+	filtered = filtered.slice(0, 25); // Hard limit of elements that can be send for autocomplete
 
 	await interaction.respond(
 		filtered.map(choice => ({ name: choice, value: choice }))
