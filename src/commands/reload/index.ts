f2632cdadd2a59c3afbf9231258aa618e9175621
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-01-17 12:43:40 +0100

    fix: deferred reply need to use editReply

--- src/commands/reload/index.ts
+++ src/commands/reload/index.ts
@@ -7,6 +7,7 @@ const reloadCommand: DiscordCommand = {
 		.setDescription("Reload every commands of the client"),
 	filters: { admin: true },
 	async execute(interaction) {
+		const deferPromise = interaction.deferReply();
 		const client = interaction.client;
 
 		const adminGuild = client.guilds.cache.get(process.env.DISCORD_ADMIN_GUILD_ID);
@@ -14,12 +15,11 @@ const reloadCommand: DiscordCommand = {
 
 		const publicGuilds = client.guilds.cache.filter((guild) => guild.id !== process.env.DISCORD_ADMIN_GUILD_ID);
 		const publicCommandsData = client.commands.filter((commands) => !commands.filters.admin).map((command) => command.data);
-		interaction.deferReply();
 		const commandSetpromises = publicGuilds.map((guild) => {
 			return guild.commands.set(publicCommandsData);
 		});
-		await Promise.all(commandSetpromises);
-		interaction.reply("Commands reloaded");
+		await Promise.all([...commandSetpromises, deferPromise]);
+		interaction.editReply("Commands reloaded");
 	}
 };
 
