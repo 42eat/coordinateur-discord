eaa4c6a6c6e09effaa4a7822c9bfb4ea0f93b36e
Saumon Brûlé <96207807+saumon-brule@users.noreply.github.com>
2026-02-07 18:39:51 +0100

    feat: beter messages for invalid input

--- src/commands/presenceShift/assertValidShift.ts
+++ src/commands/presenceShift/assertValidShift.ts
@@ -10,14 +10,20 @@ export function assertValidShift(shift: Shift) {
 
 	const now = new Date()
 
-	if (!isISO8601(shift.date.day, { strict: true }) || new Date(shift.date.day) > now) {
-		throw new ResponseError(`Are you sure about the date ? Freaking ISO format is that hard to understand ?\n# ** >>>> yyyy-MM-dd <<<<**\n-# today is \`\`${new Date().toISOString().slice(0, 10)}\`\``);
+
+	if (!members.includes(shift.referentLogin)) {
+		throw new ResponseError(`\`\`${shift.referentLogin}\`\` is not member of the **Foyer**.\nIf he should be, use the folowing command to add him.\`\`\`/add-member login:${shift.referentLogin}\`\`\``);
 	}
+	if (!isISO8601(shift.date.day, { strict: true }) || new Date(shift.date.day) > now) {
+		throw new ResponseError(`Are you sure about the date ? Are you having trouble understanding what ISO format is ? 😢\n# ** >>>> yyyy-MM-dd <<<<**\n-# If you need help like a little baby, today is \`\`${new Date().toISOString().slice(0, 10)}\`\``);
+	}
+	presences.forEach(({ login }) => {
+		if (!members.includes(login)) {
+			throw new ResponseError(`\`\`${login}\`\` is not member of the **Foyer**.\nIf he should be, use the folowing command to add him.\`\`\`/add-member login:${shift.referentLogin}\`\`\``);
+		}
+	});
 	if (!presences.some(({ login }) => login === shift.referentLogin)) {
 		throw new ResponseError("Referent must participate to the shift");
-	}
-	if (presences.some(({ login }) => !members.includes(login))) {
-		throw new ResponseError("Some participants are not members of the **foyer**");
 	}
 	if (hasDuplicate(presences.map(({ login }) => login))) {
 		throw new ResponseError("One member can't have 2 roles on the same shift");
